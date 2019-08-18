63910932f349c36ad71d18f3db558d16452cb254
J3 Laserna <j3.laserna@gmail.com>
2019-08-18 16:09:15 +0800

    set badge to zero for ios for now

--- server/init_topic.go
+++ server/init_topic.go
@@ -11,10 +11,10 @@ package main
 import (
 	"strings"
 
-	"github.com/tinode/chat/server/auth"
-	"github.com/tinode/chat/server/logs"
-	"github.com/tinode/chat/server/store"
-	"github.com/tinode/chat/server/store/types"
+	"github.com/jetri/chat/server/auth"
+	"github.com/jetri/chat/server/logs"
+	"github.com/jetri/chat/server/store"
+	"github.com/jetri/chat/server/store/types"
 )
 
 // topicInit reads an existing topic from database or creates a new topic
